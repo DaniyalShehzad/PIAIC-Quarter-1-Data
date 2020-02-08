de9f2dcaf9195f96400712f06c7d2383312b251d
Daniyal Shehzad Dar <50536147+DaniyalShehzad@users.noreply.github.com>
2020-02-08 16:10:46 +0500

    PIAIC complete quarter 1 data

---
+++ shape.py
@@ -0,0 +1,7 @@
+class Shape():
+    def __init__(self, colour, sides):
+        self.color = colour
+        self.sides = sides
+    
+    def Draw(self):
+        print("sgs")
---
+++ student.py
@@ -0,0 +1,6 @@
+class Student():
+    def __init__(self):
+        pass
+    def enroll(self):
+        print("enrolled")
+        
